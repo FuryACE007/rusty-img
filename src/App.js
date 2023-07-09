7004a2c2ed61ee2300e18e2ab254287e200850e4
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-07-09 18:29:35 +0530

    color changes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ function App() {
     <div className="flex">
       <div className=" flex-col items-center text-center mx-auto my-5">
         <h1 className="text-2xl font-semibold text-[#F8F1F1] mt-10 uppercase">
-          Creativity combined with{" "}
-          <span className="text-[#025464] border-2 border-[#E57C23] px-2 py-1 rounded-xl">
+          Image editor with{" "}
+          <span className="text-[#DDE6ED] border-2 border-[#E57C23] px-2 py-1 rounded-xl">
             the power of Rust 🔥
           </span>
         </h1>
+        <h3 className="text-lg text-slate-300 font-medium mt-10 uppercase">Select from the below options:</h3>
       </div>
-      
     </div>
   );
 }
